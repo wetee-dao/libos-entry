0d52220d35519f47b8b2ead8abe0a9681168185c
422145328@qq.com <422145328@qq.com>
2023-11-21 20:27:25 +0800

    更新基础镜像

---
+++ base-sgx/ego/build.sh
@@ -0,0 +1,14 @@
+# get shell path
+SOURCE="$0"
+while [ -h "$SOURCE"  ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+cd $DIR
+
+EGO_DEB=ego_1.4.1_amd64_ubuntu-20.04.deb  && \
+wget https://github.com/edgelesssys/ego/releases/download/v1.4.1/$EGO_DEB  && \
+
+docker build -f ./Dockerfile.ego-ubuntu-20-04 -t wetee/ubuntu-ego:20.04 .
---
+++ base-sgx/gramine/build.sh
@@ -0,0 +1,11 @@
+# get shell path
+SOURCE="$0"
+while [ -h "$SOURCE"  ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+cd $DIR
+
+docker build -f ./Dockerfile.gramine-ubuntu-20-04 -t wetee/ubuntu:20.04 .
