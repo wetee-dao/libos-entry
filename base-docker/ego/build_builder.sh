0389aa733fceb023c6cee18337657c7cdd0a99de
422145328@qq.com <422145328@qq.com>
2024-02-22 11:49:48 +0800

    update ego dockerfile

--- base-docker/ego/build_builder.sh
+++ base-docker/ego/build_builder.sh
@@ -8,5 +8,8 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
 cd $DIR
 
-docker build -f ./Dockerfile.ego-ubuntu-20-04 -t wetee/ego-ubuntu:20.04 .
-docker push wetee/ego-ubuntu:20.04
+EGO_DEB=ego_1.4.1_amd64_ubuntu-20.04.deb  && \
+wget https://github.com/edgelesssys/ego/releases/download/v1.4.1/$EGO_DEB  && \
+
+docker build -f ./Dockerfile.ego-ubuntu-20-04-builder -t wetee/ego-ubuntu-builder:20.04 .
+docker push wetee/ego-ubuntu-builder:20.04
