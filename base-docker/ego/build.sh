c9b56e20afa6214509ef079757cbc60afb1c271a
422145328@qq.com <422145328@qq.com>
2025-01-01 13:49:36 +0800

    update base image to ubuntu 24.04

--- base-docker/ego/build.sh
+++ base-docker/ego/build.sh
@@ -11,5 +11,5 @@ cd $DIR
 # EGO_DEB=ego_1.5.3_amd64_ubuntu-$(lsb_release -rs).deb
 # wget https://github.com/edgelesssys/ego/releases/download/v1.5.3/$EGO_DEB
 
-docker build -f ./Dockerfile.ego-ubuntu-22-04-deploy -t registry.cn-hangzhou.aliyuncs.com/wetee_dao/ego-ubuntu-deploy:22.04 .
-docker push registry.cn-hangzhou.aliyuncs.com/wetee_dao/ego-ubuntu-deploy:22.04
+docker build -f ./Dockerfile.ego-ubuntu-22-04-deploy -t wetee/ego-ubuntu-deploy:24.04 .
+docker push wetee/ego-ubuntu-deploy:24.04
